05b1dadf6f4d2fae259f9843c90abba1f673d407
Eudes CODO <eudescodo00@gmail.com>
2024-09-04 18:27:02 +0100

    add streamlit app with models V.2

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import joblib
+
+
+# Charger le modèle
+model = joblib.load('model_td_idf_SVC.joblib')
+#model = joblib.load('model_td_idf_RandomForestClassifier.joblib')
+#model = joblib.load('model_td_idf_regression_logistque.joblib')
+
+# Charger le vecteur
+vector = joblib.load('vector_td_idf.joblib')
 
 
 # Conteneur pour le titre et le champ de texte
@@ -20,21 +30,26 @@ def send_message():
     if st.session_state.new_message:
         # Récupérer le message saisi
         message = st.session_state.new_message
-        # Générer une réponse (ici, on génère une réponse simple)
-        prediction = '...'
-        response = f"Le sentiment de cette phrase est {prediction}"
+
+        # Mettre le message sous forme encodée
+        msg = {message}
+        msg_vectorized = vector.transform(msg)
+        prediction = model.predict(msg_vectorized)
+
+        # Générer une réponse
+        label = {0: 'sadness', 1: 'joy', 2: 'love', 3: 'anger', 4: 'fear', 5: 'surprise'}
+        sentiment = label[prediction[0]]
+
         # Afficher le message et la réponse dans le conteneur de discussion
         with chat_container:
-            st.write(f''' Vous : {message}''')
-            st.write(f"Réponse : {response}")
+            st.write(f'Vous : {message}')
+            st.write(f'''Réponse : Le sentiment de cette phrase est __{sentiment}__''')
         # Réinitialiser le champ de saisie
         st.session_state.new_message = ""
 
 # Champ de saisie pour un nouveau message dans le conteneur de saisie
 with input_container:
     st.text_input("Entrez un message:", key="new_message", on_change=send_message)
-
-
 
 
 
